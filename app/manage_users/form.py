f2a1fad74c5bcb479005cfa3f611831c32f52418
Mesele Assefa <getmesy@gmail.com>
2023-05-29 15:41:28 +0000

    update user login and registration

--- app/manage_users/form.py
+++ app/manage_users/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, EqualTo
 
 
@@ -15,5 +15,5 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    role = StringField('Role', validators=[DataRequired()])
+    role = SelectField('Role', choices=[('', 'Select Role'), ('admin', 'Admin'), ('manager', 'Manager'), ('store_keeper', 'Store Keeper'), ('others', 'Others')], validators=[DataRequired()])
     submit = SubmitField('Add User')
